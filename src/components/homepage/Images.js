c115ec76bcbd6e55a49db63a97771b2fa813b457
farid froozan <froozan@yahoo.com>
2020-04-03 23:26:15 +0430

    developer mode

--- src/components/homepage/Images.js
+++ src/components/homepage/Images.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import $ from 'jquery';
+import '../../js/jquery.toast.js';
 
 const MypicDiv = styled.div`
 	background-image: url('${process.env.PUBLIC_URL}/img/1.jpg');
@@ -11,59 +12,42 @@ const MypicDiv = styled.div`
 	position: sticky;
 `;
 
-let clicked = 0;
-let currentPic = 0;
-let rand = 0;
-let clickRate = 0;
-let firstTime = new Date();
-let debugModeSeconds = 3;
-let debugModeClick = 5;
-let myPics = [
-	{
-		background_image: `${process.env.PUBLIC_URL}/img/1.jpg`,
-		background_position: 'center'
-	},
-	{
-		background_image: `${process.env.PUBLIC_URL}/img/2.jpg`,
-		background_position: 'center'
-	},
-	{
-		background_image: `${process.env.PUBLIC_URL}/img/3.jpg`,
-		background_position: 'left'
-	},
-	{
-		background_image: `${process.env.PUBLIC_URL}/img/4.jpg`,
-		background_position: 'right'
-	}
-];
-const clickOnMyPic = () => {
-	clickRate = (new Date().getTime() - firstTime.getTime()) / 1000;
-	clicked++;
-	if (clicked % debugModeClick === 0) {
-		if (clickRate < debugModeSeconds)
-			$.toast({
-				heading: 'Cheer !',
-				text: 'Debug mode activated &#128529; ',
-				showHideTransition: 'slide'
-			});
-		else firstTime = new Date();
-	}
-	changeMyPic();
-};
-const changeMyPic = () => {
-	do {
-		rand = Math.floor(Math.random() * myPics.length);
-	} while (currentPic === rand);
+const Images = (props) => {
+	let clicked = 0;
+	let currentPic = 0;
+	let rand = 0;
+	let clickRate = 0;
+	let firstTime = new Date();
+	let debugModeSeconds = 3;
+	let debugModeClick = 5;
+	let myPics = props.images;
+	const clickOnMyPic = () => {
+		clickRate = (new Date().getTime() - firstTime.getTime()) / 1000;
+		clicked++;
+		if (clicked % debugModeClick === 0) {
+			if (clickRate < debugModeSeconds)
+				$.toast({
+					heading: 'Cheer !',
+					text: 'Debug mode activated &#128529; ',
+					showHideTransition: 'slide',
+				});
+			else firstTime = new Date();
+		}
+		changeMyPic();
+	};
+	const changeMyPic = () => {
+		do {
+			rand = Math.floor(Math.random() * myPics.length);
+		} while (currentPic === rand);
 
-	currentPic = rand;
+		currentPic = rand;
 
-	$('#mypicDiv').css({
-		'background-image': 'url("' + myPics[rand].background_image + '")',
-		'background-position': myPics[rand].background_position
-	});
-};
+		$('#mypicDiv').css({
+			'background-image': 'url("' + myPics[rand].background_image + '")',
+			'background-position': myPics[rand].background_position,
+		});
+	};
 
-function App() {
 	return (
 		<MypicDiv
 			className='col-lg-6 col-sm-12'
@@ -73,6 +57,6 @@ function App() {
 			<div id='overlay'></div>
 		</MypicDiv>
 	);
-}
+};
 
-export default App;
+export default Images;
