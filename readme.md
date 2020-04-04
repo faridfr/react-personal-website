eee072043b7b2f7cfa03f5eb026a695f97ff8756
farid froozan <froozan@yahoo.com>
2020-04-04 09:39:33 +0430

    some changes

--- readme.md
+++ readme.md
@@ -1,1 +1,8 @@
-# FaridFr.Github.io/react-personal-website
+# React simple personal website
+
+## FaridFr.Github.io/react-personal-website
+
+- Clone the project
+- Change `src/config.json`
+- `npm install`
+- `npm start`
--- src/components/homepage/EducationItem.js
+++ src/components/homepage/EducationItem.js
@@ -7,23 +7,23 @@ const EducationItem = (props) => {
 	const education = props.education;
 	return (
 		<div className='row' style={{ padding: '0px', margin: '0px' }}>
-			<div className='col-1'>
+			<div className='col-lg-1 d-none d-lg-block'>
 				<img
 					src={`${process.env.PUBLIC_URL}/${education.logo}`}
 					className='img-fluid company-logo'
 					alt='University'
 				></img>
 			</div>
-			<div className='col-8'>
+			<div className='col-lg-8 col-sm-12'>
 				<h4 style={{ fontFamily: 'farid' }}>{education.title}</h4>
 			</div>
-			<div className='col-3'>
+			<div className='col-lg-3 col-sm-12'>
 				<h6 style={{ fontFamily: 'farid', float: 'right' }}>
 					{education.time}
 				</h6>
 			</div>
 			<div
-				className='col-11 offset-1'
+				className='col-lg-11 offset-lg-1'
 				style={{ textAlign: 'justify', fontSize: '0.8rem' }}
 			>
 				<h6>{education.description}</h6>
