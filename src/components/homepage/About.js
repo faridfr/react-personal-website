ae7c48789551b42e6aca5e7e5f2fb698d4ec91ad
farid froozan <froozan@yahoo.com>
2020-04-10 13:33:04 +0430

    some changes

--- src/components/homepage/About.js
+++ src/components/homepage/About.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../App.css';
 
 const About = (props) => {
 	const about = props.about;
 	return (
-		<div className='about'>
-			<div className='title'>About Me</div>
-			<div className='description'>{about}</div>
-		</div>
+		<Fragment>
+			{about.length > 0 && (
+				<div className='about'>
+					<div className='title'>About Me</div>
+					<div className='description'>{about}</div>
+				</div>
+			)}
+		</Fragment>
 	);
 };
 
